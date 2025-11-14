8740f303a6b292db1104d6c2202d2ff9b04b59a2
mdarafatyeasin <yeasinarafat54239@gmail.com>
2025-11-14 12:37:54 +0600

    3rd commit

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 console.log("hello worlds");
 console.log("this is from mckbook")
 console.log("code is comming")
-console.log("this is the 2nd console to check the gitHub")
+console.log("this is the 3nd console to check the gitHub")
